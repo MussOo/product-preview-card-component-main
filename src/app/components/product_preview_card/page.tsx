a916196ae4de06a87f22ceade8191d550dbb159b
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2023-09-26 19:21:59 +0200

    fin card product

--- src/app/components/product_preview_card/page.tsx
+++ src/app/components/product_preview_card/page.tsx
@@ -1,16 +1,33 @@
 import styles from "./page.module.css";
-
+import cart from "./icon-cart.svg";
 export default function Preview_product() {
   return (
     <div className={styles.main}>
-      <div>IMAGES</div>
-      <div>
-        <span>Perfume</span>
-        <h3>Gabrielle Essence Eau de Parfum</h3>
-        <div>
-          <p>Description bien longue</p>
+      <div className={styles.container_image}></div>
+      <div className={styles.container_info}>
+        <span className={styles.category}>PERFUME</span>
+        <h3 className={styles.title_product}>
+          Gabrielle <br /> Essence Eau <br /> de Parfum
+        </h3>
+        <div className={styles.container_description}>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL
+          </p>
         </div>
-        <button>Add to Cart</button>
+        <div className={styles.container_prices}>
+          <span className={styles.current_price}>$149.99</span>
+          <span className={styles.old_price}>$169.99</span>
+        </div>
+        <button>
+          <img
+            width="20"
+            height="20"
+            src="https://img.icons8.com/pastel-glyph/64/FFFFFF/fast-cart.png"
+            alt="fast-cart"
+          />
+          <p>Add to Cart</p>
+        </button>
       </div>
     </div>
   );
